35e81a731e7eb92a15e4f7b218c94d344d6dc425
Marcos Fermin Lobo <marcos.fermin.lobo@cern.ch>
2016-04-26 11:56:53 +0200

    Add basic unit tests + beaker
    This patch adds basic unit, beaker tests, and fixes the package name
    to be used when installing ec2api.
    
    Change-Id: Ief9b9e440f38ba1c6bd6d148df13f9238d71bbb2

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,11 +16,19 @@
 class ec2api (
   $package_ensure = 'present',
   $package_manage = true,
-){
+) {
+
+  include ::ec2api::params
+
+  include ::ec2api::db
+  include ::ec2api::logging
+  include ::ec2api::policy
+
   if $package_manage {
     package { 'ec2api':
       ensure => $package_ensure,
-      name   => $::ec2api::params::package_manage,
+      name   => $::ec2api::params::package_name,
+      tag    => ['openstack', 'ec2api-package'],
     }
 
     Package['ec2api'] ->
---
+++ spec/acceptance/basic_ec2api_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper_acceptance'
+
+describe 'basic ec2api' do
+
+  context 'default parameters' do
+
+    it 'should work with no errors' do
+      pp= <<-EOS
+      include ::openstack_integration
+      include ::openstack_integration::repos
+      include ::openstack_integration::rabbitmq
+      include ::openstack_integration::mysql
+      include ::openstack_integration::keystone
+
+      # Ec2api resources
+      class { '::ec2api::keystone::auth':
+        password => 'a_big_secret',
+      }
+      class { '::ec2api::db::mysql':
+        password => 'a_big_secret',
+      }
+      case $::osfamily {
+        'Debian': {
+          warning('Ec2api is not yet packaged on Ubuntu systems.')
+        }
+        'RedHat': {
+          class { '::ec2api': }
+        }
+      }
+      EOS
+
+      # Run it twice to test for idempotency
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+  end
+end
