98af828779d7983ff2e2280013c17598a0f7f25e
ZhongShengping <chdzsp@163.com>
2016-04-30 10:13:32 +0800

    Test multiple operating systems for ec2api::db::postgresql
    Change-Id: If467a0013973d708c7760ad019b1cc008674b7a2

--- spec/classes/ec2api_db_postgresql_spec.rb
+++ spec/classes/ec2api_db_postgresql_spec.rb
@@ -2,21 +2,13 @@ require 'spec_helper'
 
 describe 'ec2api::db::postgresql' do
 
-  let :req_params do
-    { :password => 'pw' }
-  end
+  shared_examples_for 'ec2api::db::postgresql' do
+    let :req_params do
+      { :password => 'pw' }
+    end
 
-  let :pre_condition do
-    'include postgresql::server'
-  end
-
-  context 'on a RedHat osfamily' do
-    let :facts do
-      {
-        :osfamily                 => 'RedHat',
-        :operatingsystemrelease   => '7.0',
-        :concat_basedir => '/var/lib/puppet/concat'
-      }
+    let :pre_condition do
+      'include postgresql::server'
     end
 
     context 'with only required parameters' do
@@ -32,27 +24,19 @@ describe 'ec2api::db::postgresql' do
 
   end
 
-  context 'on a Debian osfamily' do
-    let :facts do
-      {
-        :operatingsystemrelease => '7.8',
-        :operatingsystem        => 'Debian',
-        :osfamily               => 'Debian',
-        :concat_basedir => '/var/lib/puppet/concat'
-      }
-    end
-
-    context 'with only required parameters' do
-      let :params do
-        req_params
+  on_supported_os({
+    :supported_os   => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge(OSDefaults.get_facts({
+          :processorcount => 8,
+          :concat_basedir => '/var/lib/puppet/concat'
+        }))
       end
 
-      it { is_expected.to contain_postgresql__server__db('ec2api').with(
-        :user     => 'ec2api',
-        :password => 'md58f5ac3d04c0da5a38f9c80d62f00eb1e'
-      )}
+      it_configures 'ec2api::db::postgresql'
     end
-
   end
 
 end
