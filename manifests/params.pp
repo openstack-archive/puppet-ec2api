6ba14d194ade96e2fc5eb0a8f672cb11a5b02dd7
ZhongShengping <chdzsp@163.com>
2016-05-06 13:52:19 +0800

    Fix package version and remove useless arguments
    Modify version dependencies of the openstacklib package.
    Remove useless arguments in params.pp file
    
    Change-Id: Id034fe35e12559e3f5f7b91ed57efb1afd0ea66f

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,13 +5,9 @@ class ec2api::params {
   case $::osfamily {
     'RedHat': {
       $package_name = 'openstack-ec2-api'
-      $sqlite_package_name  = undef
-      $pymysql_package_name = undef
     }
     'Debian': {
       $package_name = 'ec2api'
-      $sqlite_package_name  = 'python-pysqlite2'
-      $pymysql_package_name = 'python-pymysql'
     }
     default: {
       fail("Unsupported osfamily: ${::osfamily} operatingsystem")
