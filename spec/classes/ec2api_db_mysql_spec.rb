52a13005666b440601bd0aef768c61664dac4f8f
Takashi Kajinami <tkajinam@redhat.com>
2020-05-19 22:45:44 +0900

    Remove password hash generation in each puppet modules
    ... and migrate it to openstacklib so that all logics about database
    configuration are implemented in one common place.
    
    Depends-on: https://review.opendev.org/#/c/728595/
    Change-Id: I900142c35e8fb20d3279f607cdd80ad53157531c

--- spec/classes/ec2api_db_mysql_spec.rb
+++ spec/classes/ec2api_db_mysql_spec.rb
@@ -1,64 +1,71 @@
 require 'spec_helper'
 
 describe 'ec2api::db::mysql' do
-  on_supported_os(supported_os: OSDefaults.get_supported_os).each do |os,facts|
-    context "on #{os}" do
+  shared_examples_for 'ec2api::db::mysql' do
 
-      let(:facts) { facts.merge! @default_facts }
-
-      describe 'with default parameters' do
-        let(:params) do
-          {
-              password: 'my_password',
-          }
-        end
-
-        db_parameters = {
-            user: 'ec2api',
-            password_hash: '*CCD3A959D6A004B9C3807B728BC2E55B67E10518',
-            dbname: 'ec2api',
-            host: '127.0.0.1',
-            charset: 'utf8',
-            collate: 'utf8_general_ci',
+    context 'with default parameters' do
+      let(:params) do
+        {
+            password: 'ec2apipass',
         }
-
-        it { is_expected.to compile.with_all_deps }
-
-        it { is_expected.to contain_class('ec2api::db::mysql') }
-
-        it { is_expected.to contain_openstacklib__db__mysql('ec2api').with(db_parameters) }
       end
 
-      describe 'with custom parameters' do
-        let(:params) do
-          {
-              password: 'my_password',
-              user: 'ec2user',
-              dbname: 'ec2db',
-              host: '192.168.0.1',
-              charset: 'cp1251',
-              collate: 'cp1251_general_ci',
-              allowed_hosts: %w(192.168.0.2 192.168.0.3),
-          }
-        end
+      db_parameters = {
+          user: 'ec2api',
+          password: 'ec2apipass',
+          dbname: 'ec2api',
+          host: '127.0.0.1',
+          charset: 'utf8',
+          collate: 'utf8_general_ci',
+      }
 
-        db_parameters = {
+      it { is_expected.to compile.with_all_deps }
+
+      it { is_expected.to contain_class('ec2api::db::mysql') }
+
+      it { is_expected.to contain_openstacklib__db__mysql('ec2api').with(db_parameters) }
+    end
+
+    context 'with custom parameters' do
+      let(:params) do
+        {
+            password: 'ec2apipass',
             user: 'ec2user',
-            password_hash: '*CCD3A959D6A004B9C3807B728BC2E55B67E10518',
             dbname: 'ec2db',
             host: '192.168.0.1',
             charset: 'cp1251',
             collate: 'cp1251_general_ci',
-            allowed_hosts: %w(192.168.0.2 192.168.0.3)
+            allowed_hosts: %w(192.168.0.2 192.168.0.3),
         }
-
-        it { is_expected.to compile.with_all_deps }
-
-        it { is_expected.to contain_class('ec2api::db::mysql') }
-
-        it { is_expected.to contain_openstacklib__db__mysql('ec2api').with(db_parameters) }
       end
 
+      db_parameters = {
+          user: 'ec2user',
+          password: 'ec2apipass',
+          dbname: 'ec2db',
+          host: '192.168.0.1',
+          charset: 'cp1251',
+          collate: 'cp1251_general_ci',
+          allowed_hosts: %w(192.168.0.2 192.168.0.3)
+      }
+
+      it { is_expected.to compile.with_all_deps }
+
+      it { is_expected.to contain_class('ec2api::db::mysql') }
+
+      it { is_expected.to contain_openstacklib__db__mysql('ec2api').with(db_parameters) }
     end
+  end
+
+  on_supported_os({
+    :supported_os => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts())
+      end 
+
+      it_behaves_like 'ec2api::db::mysql'
+    end 
   end
 end
