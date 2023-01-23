f5c271ae44c4daba6b8e151c030304764aaf4dc0
Takashi Kajinami <tkajinam@redhat.com>
2023-01-23 14:21:21 +0900

    Expose policy_default_rule
    The option has been managed by the underlying puppet-oslo module but
    has not been configurable. This introduces the parameter to customize
    the option.
    
    Change-Id: I2382514189617edb9454c3d9e8b5f15de9c96237

--- spec/classes/ec2api_policy_spec.rb
+++ spec/classes/ec2api_policy_spec.rb
@@ -9,6 +9,7 @@ describe 'ec2api::policy' do
           :enforce_scope        => false,
           :enforce_new_defaults => false,
           :policy_path          => '/etc/ec2api/policy.yaml',
+          :policy_default_rule  => 'default',
           :policy_dirs          => '/etc/ec2api/policy.d',
           :policies             => {
             'context_is_admin' => {
@@ -37,6 +38,7 @@ describe 'ec2api::policy' do
           :enforce_scope        => false,
           :enforce_new_defaults => false,
           :policy_file          => '/etc/ec2api/policy.yaml',
+          :policy_default_rule  => 'default',
           :policy_dirs          => '/etc/ec2api/policy.d',
         )
       end
